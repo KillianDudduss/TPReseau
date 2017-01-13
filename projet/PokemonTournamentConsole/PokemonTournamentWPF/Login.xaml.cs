84d77047c3a5bd731d37d7085e754f728c1d8985
MohamedLoukili <mohamed.loukili.19@gmail.com>
2017-01-13 15:53:30 +0100

    Add files via upload

---
+++ projet/PokemonTournamentConsole/PokemonTournamentWPF/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokemonTournamentWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (BusinessLayer.BusinessManager.CheckConnexionUser(log.Text.ToString(), pass.Password))
+            {
+                MainWindow win = new MainWindow();
+                win.Show();
+                this.Close;
+            }
+        }
+    }
+}
