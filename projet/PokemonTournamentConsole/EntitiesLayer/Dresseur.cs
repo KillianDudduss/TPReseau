a158ac7ce211d5474e14dadc91ec4efebfb8ec29
Zzpiraten <oudinbenjamin@hotmail.fr>
2017-01-13 15:38:21 +0100

    Add files via upload

---
+++ projet/PokemonTournamentConsole/EntitiesLayer/Dresseur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer {
+	public class Dresseur : EntityObject {
+		private String _nom;
+		private String Nom {
+			get {
+				return _nom;
+			}
+			set {
+				_nom = Nom;
+			}
+		}
+		private int Score;
+
+		public Dresseur(String nom) {
+			_nom = nom;
+			Score = 0;
+		}
+
+		public override string ToString() {
+			return "Dresseur : " + Nom + " | score : " + Score + " points";
+		}
+	}
+}
