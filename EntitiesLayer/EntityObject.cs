9560d71474a080b2034f2749761a2703c36af126
kidusautie <kidusautie@A212PC08>
2017-01-20 16:13:14 +0100

    Ajoutez des fichiers projet.

---
+++ EntitiesLayer/EntityObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer
+{
+    abstract public class EntityObject
+    {
+		static private int _compteur = 0;
+		protected int ID;
+
+		public EntityObject() {
+			ID = _compteur++;
+		}
+
+		static public int get_compteur() {
+			return _compteur;
+		}
+    }
+}
---
+++ EntitiesLayer/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer {
+	public enum ETypeElement {
+		Eau, Feu, Terre, Insecte, Plante, Tonnerre
+	}
+
+	public enum ECaracteristique {
+		VIE, FORCE, AGILITE, ENDURANCE, VITESSE
+	}
+
+	public enum EPhaseTournoi {
+		QuartFinale, HuitiemeFinale, DemiFinale, Finale
+	}
+}
